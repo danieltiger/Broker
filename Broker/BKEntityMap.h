60323e3084659a458b3e4dbcac35560ee9798753
Andrew Smith <drewsmits@gmail.com>
2011-10-05 17:57:28 -0700

    Added entity map

---
+++ Broker/BKEntityMap.h
@@ -0,0 +1,13 @@
+//
+//  BKEntityMap.h
+//  Broker
+//
+//  Created by Andrew Smith on 10/5/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface BKEntityMap : NSObject
+
+@end
